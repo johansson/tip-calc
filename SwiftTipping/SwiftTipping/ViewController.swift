d6ed7b92238c3dfe7f1841be59784b822db8e581
Will Johansson <will.johansson@rd.io>
2015-04-13 22:21:06 -0700

    Initial project

---
+++ SwiftTipping/SwiftTipping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTipping
+//
+//  Created by Will Johansson on 2015-04-13.
+//  Copyright (c) 2015 Will Johansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

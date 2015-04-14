c914146c52dcd355461d41c6e7a477748d6dd029
Will Johansson <will.johansson@rd.io>
2015-04-14 13:32:30 -0700

    limit editing for currency.

--- SwiftTipping/SwiftTipping/CalculatorViewController.swift
+++ SwiftTipping/SwiftTipping/CalculatorViewController.swift
@@ -38,7 +38,9 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func onBillAmountChanged(sender: UITextField) {
-        billAmt = (sender.text as NSString).floatValue
+        let text = (sender.text as NSString)
+
+        billAmt = (text.substringFromIndex(1) as NSString).floatValue
         updateFields()
     }
     
@@ -51,7 +53,21 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
 
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 
-        return true;
+        let text = (textField.text as NSString)
+        
+        // don't delete the $
+        if (text.length == 1 && string == "") { return false }
+        
+        // don't add another decimal
+        if (text.containsString(".") && string == ".") { return false }
+
+        // don't blow up
+        if (string != "" && text.length >= 1 && text.length <= 2) { return true }
+        
+        // don't add more than 2 decimal places
+        if (string != "" && text.substringFromIndex(text.length - 3).hasPrefix(".")) { return false }
+        
+        return true
     }
     
     @IBAction func onTap(sender: AnyObject) {
